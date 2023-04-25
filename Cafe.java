a19adb7c4a7cb67fd257d97336968020dc951722
hala <hmaloul@smith.edu>
2023-04-25 18:30:10 -0400

    edits

--- Cafe.java
+++ Cafe.java
@@ -1,9 +1,13 @@
 /* This is a stub for the Cafe class */
 public class Cafe extends Building{
-    private int nCoffeeOunces; // The number of ounces of coffee remaining in inventory
-    private int nSugarPackets; // The number of sugar packets remaining in inventory
-    private int nCreams; // The number of "splashes" of cream remaining in inventory
-    private int nCups; // The number of cups remaining in inventory
+    //*The number of ounces of coffee remaining in inventory*/
+    private int nCoffeeOunces;
+    //*  The number of sugar packets remaining in inventory*/
+    private int nSugarPackets;
+     //* The number of "splashes" of cream remaining in inventory*/
+    private int nCreams; 
+    //* The number of cups remaining in inventory*/
+    private int nCups; 
     public Cafe(String name,String address, int nFloors,int nCoffeeOunces,int nSugarPackets,int nCreams,int nCups ) {
         // System.out.println("You have built a cafe: ☕");
         super(name,address,nFloors);
